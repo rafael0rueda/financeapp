3d51800dee87701863bac6792bfcf50f42ae02f8
Rafael Rueda <rrrm85@gmail.com>
2025-05-21 21:36:47 -0500

    add categories

--- main.py
+++ main.py
@@ -6,6 +6,37 @@ import os
 
 st.set_page_config(page_title="Finance Dashboard", page_icon="💰", layout="wide")
 
+category_file = "categories.json"
+
+if "categories" not in st.session_state:
+    st.session_state.categories = {
+        "Uncategorized": []
+    }
+
+if os.path.exists(category_file):
+    with open(category_file, "r") as f:
+        st.session_state.categories = json.load(f)
+
+def save_categories():
+    with open(category_file, "w") as f:
+        json.dump(st.session_state.categories, f)
+
+def categorize_transactions(df):
+    df["Category"] = "Uncategorized"
+
+    for category, keywords in st.session_state.categories.items():
+        if category == "Uncategorized" or not keywords:
+            continue
+        
+        lowered_keywords = [keyword.lower().strip() for keyword in keywords]
+
+        for idx, row in df.iterrows():
+            details = row["Details"].lower().strip()
+            if details in lowered_keywords:
+                df.at[idx, "Category"] = category
+
+    return df
+
 def load_transactions(file):
     try:
         df = pd.read_csv(file)
@@ -14,10 +45,19 @@ def load_transactions(file):
         df["Amount"] = df["Amount"].astype(float)
         df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
         #st.write(df)
-        return df
+        return categorize_transactions(df)
+
     except Exception as e:
         st.error(f"Error processing file: {str(e)}")
         return None
+
+def add_keyword_to_category(category, keyword):
+    keyword = keyword.strip()
+    if keyword and keyword not in st.session_state.categories[category]:
+        st.session_state.categories[category].append(keyword)
+        save_categories()
+        return True
+    return False
 
 def main():
     st.title("Finance Dashboard")
@@ -31,9 +71,39 @@ def main():
             debits_df = df[df["Debit/Credit"] == "Debit"].copy()
             credit_df = df[df["Debit/Credit"] == "Credit"].copy()
 
+            st.session_state.debits_df = debits_df.copy()
+
             tab1, tab2 = st.tabs(["Expenses (Debits)", "Credits"])
             with tab1:
-                st.write(debits_df)
+                new_category = st.text_input("New Category Name")
+                add_buttom = st.button("Add Category")
+
+                if add_buttom and new_category:
+                    if new_category not in st.session_state.categories:
+                        st.session_state.categories[new_category] = []
+                        save_categories()
+                        st.rerun()
+
+                st.subheader("Your Expenses")
+                edited_df = st.data_editor(
+                        st.session_state.debits_df[["Date", "Details", "Amount", "Category"]],
+                        column_config= {
+                            "Date": st.column_config.DateColumn("Date", format="DD/MM/YYYY"),
+                            "Amount": st.column_config.NumberColumn("Amount", format="%.2f COP"),
+                            "Category": st.column_config.SelectboxColumn(
+                                "Category",
+                                options=list(st.session_state.categories.keys())
+                            )
+                    },
+                    hide_index=True,
+                    use_container_width=True,
+                    key="category_editor"
+                )
+
+                save_button = st.button("Apply Changes", type="primary")
+                if save_button:
+                    pass
+
             with tab2:
                 st.write(credit_df)
 
